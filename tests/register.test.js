b1d953956135b589a61870f5e28c1e2b9b1d0d7b
imbrionofsocks <mefbysl@gmail.com>
2024-04-10 16:17:56 +0300

    upd register.test file

--- tests/register.test.js
+++ tests/register.test.js
@@ -2,21 +2,15 @@ const { Builder } = require('selenium-webdriver');
 const RegisterPage = require('/pages/RegisterPage');
 
 describe('Register Page tests', () => {
-    let driver;
-    let homePage;
+
 
     beforeAll(async () => {
-        driver = await new Builder().forBrowser('chrome').build();
-        homePage = new RegisterPage(driver);
     });
 
     afterAll(async () => {
-        await driver.quit();
     });
 
     test('should open home page and click some element', async () => {
-        await homePage.open();
-        await homePage.clickSomeElement();
-        // Добавьте здесь проверки
     });
+
 });
